1941f6cec06f02085c9e2fdd11892d3a4daf0a78
Neeraj Dalal <admin@nrjdalal.com>
2025-05-20 13:12:00 +0530

    chore: tweaks > access.tsx better-auth.json index.ts registry.json client.ts .example.env
    src/components/access.tsx
    bun.lock
    public/r/better-auth.json
    src/lib/auth/index.ts
    public/r/registry.json
    src/lib/auth/client.ts
    .example.env
    package.json

--- src/components/access.tsx
+++ src/components/access.tsx
@@ -2,9 +2,12 @@
 
 import { useState } from "react"
 
+import { zodResolver } from "@hookform/resolvers/zod"
 import { RiGithubFill, RiGoogleFill } from "@remixicon/react"
 import { GalleryVerticalEnd, Loader2 } from "lucide-react"
+import { useForm } from "react-hook-form"
 import { toast } from "sonner"
+import * as z from "zod"
 
 import { signIn } from "@/lib/auth/client"
 import { Button } from "@/components/ui/button"
@@ -15,11 +18,47 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+
+const formSchema = z.object({
+  email: z.string().min(2, {
+    message: "Please enter a valid email address!",
+  }),
+})
 
 export default function Component() {
   const [loader, setLoader] = useState<"email" | "github" | "google" | null>(null)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  })
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setLoader("email")
+    const res = await signIn.magicLink({
+      email: data.email,
+      callbackURL: "/x",
+    })
+    if (res.error) {
+      toast.error(res.error.message)
+      setLoader(null)
+    } else {
+      toast.success("Check your email for the magic link!")
+      setLoader(null)
+    }
+    form.reset()
+  }
 
   return (
     <Dialog>
@@ -32,93 +71,96 @@ export default function Component() {
         <DialogHeader className="sr-only">
           <DialogTitle className="text-center">Login</DialogTitle>
         </DialogHeader>
-        <div className="w-full">
-          <div className="flex flex-col gap-6">
-            <form>
-              <div className="flex flex-col gap-6">
-                <div className="flex flex-col items-center gap-2">
-                  <a href="#" className="flex flex-col items-center gap-2 font-medium">
-                    <div className="flex size-8 items-center justify-center rounded-md">
-                      <GalleryVerticalEnd className="size-6" />
-                    </div>
-                    <span className="sr-only">ACME Inc.</span>
-                  </a>
-                  <h1 className="text-xl font-bold">Welcome to ACME Inc.</h1>
-                </div>
-                <div className="flex flex-col gap-6">
-                  <div className="grid gap-3">
-                    <Label htmlFor="email">Email</Label>
-                    <Input id="email" type="email" placeholder="m@example.com" required />
-                  </div>
-                  <Button
-                    type="submit"
-                    className="w-full cursor-pointer"
-                    onClick={async () => {
-                      setLoader("email")
-                      toast.info("Login with email is not implemented yet!")
-                      setLoader(null)
-                    }}
-                    disabled={loader === "email"}
-                  >
-                    {loader === "email" ? <Loader2 className="size-5 animate-spin" /> : null}
-                    Login
-                  </Button>
-                </div>
-                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-                  <span className="bg-background text-muted-foreground relative z-10 px-2">Or</span>
-                </div>
-                <div className="grid gap-4">
-                  <Button
-                    variant="outline"
-                    type="button"
-                    className="w-full cursor-pointer"
-                    onClick={async () => {
-                      setLoader("github")
-                      const res = await signIn.social({
-                        provider: "github",
-                        callbackURL: "/x",
-                      })
-                      if (res.error) {
-                        toast.error(res.error.message)
-                        setLoader(null)
-                      }
-                    }}
-                    disabled={loader === "github"}
-                  >
-                    {loader === "github" ? (
-                      <Loader2 className="size-5 animate-spin" />
-                    ) : (
-                      <RiGithubFill className="size-5" />
-                    )}
-                    Continue with Github
-                  </Button>
-                  <Button
-                    variant="outline"
-                    type="button"
-                    className="w-full cursor-pointer"
-                    onClick={async () => {
-                      setLoader("google")
-                      const res = await signIn.social({
-                        provider: "google",
-                        callbackURL: "/x",
-                      })
-                      if (res.error) {
-                        toast.error(res.error.message)
-                        setLoader(null)
-                      }
-                    }}
-                    disabled={loader === "google"}
-                  >
-                    {loader === "google" ? (
-                      <Loader2 className="size-5 animate-spin" />
-                    ) : (
-                      <RiGoogleFill className="size-5" />
-                    )}
-                    Continue with Google
-                  </Button>
-                </div>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col items-center gap-2">
+            <div className="flex flex-col items-center gap-2 font-medium">
+              <div className="flex size-8 items-center justify-center rounded-md">
+                <GalleryVerticalEnd className="size-6" />
               </div>
+              <span className="sr-only">ACME Inc.</span>
+            </div>
+            <h1 className="text-xl font-semibold">Welcome to ACME Inc.</h1>
+          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col items-center gap-y-3">
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        className="text-center focus:placeholder:opacity-0"
+                        placeholder="admin@nrjdalal.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" variant="secondary" className="w-full cursor-pointer">
+                {loader === "email" ? <Loader2 className="size-5 animate-spin" /> : null}
+                Login
+              </Button>
             </form>
+          </Form>
+          <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+            <span className="bg-background text-muted-foreground relative z-10 px-2 text-xs">
+              OR
+            </span>
+          </div>
+          <div className="grid gap-4">
+            <Button
+              variant="outline"
+              type="button"
+              className="w-full cursor-pointer"
+              onClick={async () => {
+                setLoader("github")
+                const res = await signIn.social({
+                  provider: "github",
+                  callbackURL: "/x",
+                })
+                if (res.error) {
+                  toast.error(res.error.message)
+                  setLoader(null)
+                }
+              }}
+              disabled={loader === "github"}
+            >
+              {loader === "github" ? (
+                <Loader2 className="size-5 animate-spin" />
+              ) : (
+                <RiGithubFill className="size-5" />
+              )}
+              Continue with Github
+            </Button>
+            <Button
+              variant="outline"
+              type="button"
+              className="w-full cursor-pointer"
+              onClick={async () => {
+                setLoader("google")
+                const res = await signIn.social({
+                  provider: "google",
+                  callbackURL: "/x",
+                })
+                if (res.error) {
+                  toast.error(res.error.message)
+                  setLoader(null)
+                }
+              }}
+              disabled={loader === "google"}
+            >
+              {loader === "google" ? (
+                <Loader2 className="size-5 animate-spin" />
+              ) : (
+                <RiGoogleFill className="size-5" />
+              )}
+              Continue with Google
+            </Button>
             <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
               By clicking continue, you agree to our <a href="#">Terms of Service</a> and{" "}
               <a href="#">Privacy Policy</a>.
--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,7 +1,8 @@
 import { db } from "@/db"
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
-import { nextCookies } from "better-auth/next-js"
+import { magicLink } from "better-auth/plugins"
+import { Resend } from "resend"
 
 import { account, session, user, verification } from "@/db/schema/auth"
 
@@ -22,6 +23,17 @@ export const auth = betterAuth({
     },
   },
   plugins: [
-    nextCookies(), // for email access
+    magicLink({
+      sendMagicLink: async ({ email, url }) => {
+        const resend = new Resend(process.env.RESEND_API_KEY as string)
+
+        await resend.emails.send({
+          from: "ACME Inc. <onboarding@tns.nrjdalal.com>",
+          to: [email],
+          subject: "Verify your email address",
+          html: `Click the link to verify your email: ${url}`,
+        })
+      },
+    }),
   ],
 })
